3a7eada364c07c0ff06a843b95edc41071698fe3
Eliza Thomas <elizamthomas1994@gmail.com>
2022-12-14 17:38:39 +0000

    Modified tests for all classes to ensure cleaner code

--- phase_two/test_drive/shoppingBasket/candy.test.js
+++ phase_two/test_drive/shoppingBasket/candy.test.js
@@ -1,13 +1,15 @@
 const Candy = require('./Candy');
 
-it('returns name of candy', () => {
-  const candy = new Candy('Mars', 4.99)
+describe('Candy', () => {
+  it('returns name of candy', () => {
+    const candy = new Candy('Mars', 4.99)
 
-  expect(candy.getName()).toBe('Mars');
-});
+    expect(candy.getName()).toBe('Mars');
+  });
 
-it('returns price of candy', () => {
-  const candy = new Candy('Mars', 4.99)
+  it('returns price of candy', () => {
+    const candy = new Candy('Mars', 4.99)
 
-  expect(candy.getPrice()).toBe(4.99);
+    expect(candy.getPrice()).toBe(4.99);
+  });
 });
--- phase_two/test_drive/shoppingBasket/shoppingBasket.test.js
+++ phase_two/test_drive/shoppingBasket/shoppingBasket.test.js
@@ -1,34 +1,35 @@
 const ShoppingBasket = require('./shoppingBasket');
 const Candy = require('./Candy');
 
+describe('ShoppingBasket', () => {
+  it('adds candy to the basket', () =>  {
+    let basket = new ShoppingBasket();
 
-it('adds candy to the basket', () =>  {
-  let basket = new ShoppingBasket();
+    const candy = new Candy('Mars', 4.99);
 
-  const candy = new Candy('Mars', 4.99);
+    basket.addItem(candy);
 
-  basket.addItem(candy);
-
-  expect(basket.candies).toEqual([ {"name": 'Mars', "price": 4.99 } ]);
-});
+    expect(basket.candies).toEqual([ {"name": 'Mars', "price": 4.99 } ]);
+  });
 
 
 
-it('outputs total price value as 0 if basket is empty', () => {
-  let basket = new ShoppingBasket();
+  it('outputs total price value as 0 if basket is empty', () => {
+    let basket = new ShoppingBasket();
 
-  expect(basket.getTotalPrice()).toBe(0);
-});
+    expect(basket.getTotalPrice()).toBe(0);
+  });
 
-it('outputs total price of added candies', () => {
-  let basket = new ShoppingBasket();
+  it('outputs total price of added candies', () => {
+    let basket = new ShoppingBasket();
 
-  const candy = new Candy('Mars', 4.99);
+    const candy = new Candy('Mars', 4.99);
 
-  basket.addItem(candy);
-  basket.addItem(candy);
-  basket.addItem(candy);
-  basket.addItem(candy);
+    basket.addItem(candy);
+    basket.addItem(candy);
+    basket.addItem(candy);
+    basket.addItem(candy);
 
-  expect(basket.getTotalPrice()).toBe(19.96);
+    expect(basket.getTotalPrice()).toBe(19.96);
+  });
 });
